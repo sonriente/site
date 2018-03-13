f8674809053163ef7a8808b10698ba642c9cf8da
Olena Romanenko <oriendri@gmail.com>
2018-03-13 13:45:17 +0200

    Create Request.php

---
+++ Library/Request.php
@@ -0,0 +1,57 @@
+<?php
+
+class Request
+{
+    private $get = array();
+    private $post = array();
+
+    /**
+     * Request constructor.
+     */
+    public function __construct()
+    {
+        $this->get = $_GET;
+        $this->post = $_POST;
+    }
+
+
+    /**
+     * @param $key
+     * @param null $default
+     * @return null
+     */
+    public function get($key, $default = null)
+    {
+        if (isset($this->get[$key])) {
+            return $this->get[$key];
+        }
+
+        return $default;
+    }
+
+
+    /**
+     * @param $key
+     * @param null $default
+     * @return null
+     */
+    public function post($key, $default = null)
+    {
+        if (isset($this->post[$key])) {
+            return $this->post[$key];
+        }
+
+        return $default;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function isPost()
+    {
+        return (bool)$_POST;
+    }
+
+
+}
