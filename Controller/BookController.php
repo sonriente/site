747b0a1ca6017c3c98996a928a51f1cecdd289b8
Olena Romanenko <oriendri@gmail.com>
2018-03-13 13:46:13 +0200

    Create BookController.php

---
+++ Controller/BookController.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Гость
+ * Date: 29.12.2015
+ * Time: 20:03
+ */
+class BookController
+{
+    public function indexAction(Request $request)
+    {
+        return 4;
+    }
+}
